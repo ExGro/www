af42017f41f41835157e6b7d93d5309f549130c2
exgro <gro_ftf@mail.ru>
2022-05-10 12:09:16 +0600

    commit #1

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>www - сайт для разработчиков</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css" charset="utf-8">
+	<meta name="description" content="Информационно-развлекательный портал для программимтов! Обучающие курсы, новости в сфере IT и многое другое">
+	<meta name="keywords" content="www, it, курсы, новости, уроки">
+	<link rel="shortcut icon" type="image/x-icon" href="img/favico.ico">
+	<script src="https://kit.fontawesome.com/1feb837ae7.js" crossorigin="anonymous"></script>
+</head>
+<body>
+	<header>
+		<div id="logo">
+			<a href="https://www/" title="На главную">
+				<img src="img/logo.png" title="www" alt="www">
+				<span>www</span>
+			</a>
+		</div>
+		<div id="about">
+			<a href="" title="Узнать больше об рекламе">Реклама</a>
+			<a href="" title="Написать нам письмо">Обратная связь</a>
+		</div>
+		<div id="reg_auth">
+			<a href="" title="Войти в кабинет пользователя">
+				<div id="btn">
+					Войти
+				</div>
+			</a>
+			<a href="" title="Зарегистрироваться на сайте">
+				<div id="btn">
+					Регистрация
+				</div>
+			</a>
+		</div>
+	</header>
+	<nav>
+		<div id="menuShow"><i class="fa-solid fa-bars"></i></div>
+		<div id="hideMenu">
+			<a href="">IT новости</a>
+			<a href="">Видео курсы</a>
+			<a href="">Сайты на заказ</a>
+			<a href="">Форум</a>
+			<a href="">IT задачи</a>
+		</div>
+		<div id="search">
+			<span>Поиск</span>
+			<i class="fa-solid fa-magnifying-glass"></i>
+		</div>
+	</nav>
+</body>
+</html>
